702dc3ff92da3e878088f3226d9202579313d560
Alehman74 <Alehman74@yahoo.com>
2021-12-02 14:45:00 -0600

    added players, batting, and pitching

--- GiveUsAnADrSpeegle.sql
+++ GiveUsAnADrSpeegle.sql
@@ -7,7 +7,9 @@ DROP TABLE IF EXISTS People;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Parks;
 DROP TABLE IF EXISTS WorldSeries;
-
+DROP TABLE IF EXISTS Players;
+DROP TABLE IF EXISTS BattingStats;
+DROP TABLE IF EXISTS PitchingStats;
 
 CREATE TABLE Teams (
 	year int(11) NOT NULL,
@@ -26,12 +28,8 @@ CREATE TABLE People (
     personID varchar(9) NOT NULL,
     nameFirst varchar(255),
     nameLast varchar(255),
-    birthYear int(11),
-    birthMonth int(11),
-    birthDay int(11),
-    deathYear int(11),
-    deathMonth int(11),
-    deathDay int(11),
+    birthDate date,
+    deathDate date,
     weight int(11),
     height int(11),
     birthCountry varchar(255),
@@ -40,7 +38,8 @@ CREATE TABLE People (
     deathCountry varchar(255),
     deathState varchar(255),
     deathCity varchar(255),
-    #salary double,
+    debut date,
+    finalGame date,
 
     primary key (personID)
 );
@@ -71,3 +70,73 @@ CREATE TABLE WorldSeries(
 
     primary key (year)
 );
+
+CREATE TABLE Players(
+    personID varchar(9) NOT NULL,
+    year smallint(6) NOT NULL,
+    stint smallint(6) NOT NULL,
+    battingID varchar(23),
+    pitchingID varchar(23),
+    fieldingID varchar(23),
+
+    primary key (personID, year, stint)
+);
+
+
+CREATE TABLE BattingStats(
+    battingID varchar(23) NOT NULL,
+    teamID char(3),
+    G smallint(6),
+    AB smallint(6),
+    R smallint(6),
+    H smallint(6),
+    twoB smallint(6),
+    threeB smallint(6),
+    HR smallint(6),
+    RBI smallint(6),
+    SB smallint(6),
+    CS smallint(6),
+    BB smallint(6),
+    SO smallint(6),
+    IBB smallint(6),
+    HBP smallint(6),
+    SH smallint(6),
+    SF smallint(6),
+    GIDP smallint(6),
+
+    primary key (battingID)
+);
+
+CREATE TABLE PitchingStats(
+    pitchingID varchar(23) NOT NULL,
+    teamID char(3),
+    W smallint(6),
+    L smallint(6),
+    G smallint(6),
+    GS smallint(6),
+    CG smallint(6),
+    SHO smallint(6),
+    SV smallint(6),
+    IPouts int(11),
+    H smallint(6),
+    ER smallint(6),
+    HR smallint(6),
+    BB smallint(6),
+    SO smallint(6),
+    BAOpp double,
+    ERA double,
+    IBB  smallint(6),
+    WP smallint(6),
+    HBP smallint(6),
+    BK smallint(6),
+    BFP smallint(6),
+    GF smallint(6),
+    R smallint(6),
+    SH smallint(6),
+    SF smallint(6),
+    GIDP smallint(6),
+
+    primary key (pitchingID)
+);
+
+
--- PeopleInit.py
+++ PeopleInit.py
@@ -1,8 +1,9 @@
 import sys
+import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, Numeric, MetaData, create_engine
-from strFuncs import emptyIntNone, emptyStrNone, emptyFloatNone
+from sqlalchemy import Column, Integer, String, Numeric, create_engine, Date
+from strFuncs import *
 
 Base = declarative_base()
 
@@ -12,12 +13,14 @@ class Person(Base):
     personID = Column(String(9), primary_key=True)
     nameFirst = Column(String(255))
     nameLast = Column(String(255))
-    birthYear = Column(Integer)
-    birthMonth = Column(Integer)
-    birthDay = Column(Integer)
-    deathYear = Column(Integer)
-    deathMonth = Column(Integer)
-    deathDay = Column(Integer)
+    birthDate = Column(Date)
+    deathDate = Column(Date)
+    #birthYear = Column(Integer)
+    #birthMonth = Column(Integer)
+    #birthDay = Column(Integer)
+    #deathYear = Column(Integer)
+    #deathMonth = Column(Integer)
+    #deathDay = Column(Integer)
     weight = Column(Integer)
     height = Column(Integer)
     birthCountry = Column(String(255))
@@ -27,6 +30,9 @@ class Person(Base):
     deathState = Column(String(255))
     deathCity = Column(String(255))
     #salary = Column(Numeric)
+    debut = Column(Date)
+    finalGame = Column(Date)
+
 
 
     def __init__(self, line):
@@ -35,12 +41,14 @@ class Person(Base):
         self.personID = emptyStrNone(data[0])
         self.nameFirst = emptyStrNone(data[13])
         self.nameLast = emptyStrNone(data[14])
-        self.birthYear = emptyIntNone(data[1])
-        self.birthMonth = emptyIntNone(data[2])
-        self.birthDay = emptyIntNone(data[3])
-        self.deathYear = emptyIntNone(data[7])
-        self.deathMonth = emptyIntNone(data[8])
-        self.deathDay = emptyIntNone(data[9])
+        self.birthDate = emptyDateNone(data[1], data[2], data[3])
+        self.deathDate = emptyDateNone(data[7], data[8], data[9])
+        #self.birthYear = emptyIntNone(data[1])
+        #self.birthMonth = emptyIntNone(data[2])
+        #self.birthDay = emptyIntNone(data[3])
+        #self.deathYear = emptyIntNone(data[7])
+        #self.deathMonth = emptyIntNone(data[8])
+        #self.deathDay = emptyIntNone(data[9])
         self.weight = emptyIntNone(data[16])
         self.height = emptyIntNone(data[17])
         self.birthCountry = emptyStrNone(data[4])
@@ -49,6 +57,17 @@ class Person(Base):
         self.deathCountry = emptyStrNone(data[10])
         self.deathState = emptyStrNone(data[11])
         self.deathCity = emptyStrNone(data[12])
+        dateStr = data[20].split('-')
+        try:
+            self.debut = emptyDateNone(dateStr[0], dateStr[1], dateStr[2])
+        except Exception:
+            self.debut = None
+
+        dateStr = data[21].split('-')
+        try:
+            self.finalGame = emptyDateNone(dateStr[0], dateStr[1], dateStr[2])
+        except Exception:
+            self.finalGame = None
         #self.salary = emptyStrNone(data[])
 
 def initPeopleTable():
