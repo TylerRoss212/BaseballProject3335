c9af7869655d5546f52449db65dd381140d1b36f
JoshuaHunter16 <jchunter816@gmail.com>
2021-12-02 15:01:56 -0600

    Fixing rerouting for signupsuccess

--- app.py
+++ app.py
@@ -14,9 +14,35 @@ def form():
 def signup():
     return render_template('signup.html')
 
+@app.route('/signupSuccess', methods=['POST', 'GET'])
+def signupSuccess():
+    if request.method == 'POST':
+        form_data = request.form
+
+        params = []
+        params.append(form_data['username'])
+        params.append(hashlib.sha256(form_data['password'].encode('utf-8')).hexdigest())
+
+        try:
+            sql = 'INSERT INTO Users (username, password) VALUES (%s, %s)'
+            print(sql)
+            cur.execute(sql, params)
+
+        except Exception:
+            con.rollback()
+            print("Database Exception")
+            raise
+        else:
+            con.commit()
+        finally:
+            con.close()
+
+        return render_template('signupSuccess.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
         
 @app.route('/dashboard', methods=['POST', 'GET'])
 def dashboard():
